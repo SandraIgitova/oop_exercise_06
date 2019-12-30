6af3af41f08da11d183c5d0b2d3aff410ab7a0d6
Sandra <48455689+SandraIgitova@users.noreply.github.com>
2019-12-30 14:26:58 +0300

    Add files via upload

---
+++ TIterator.h
@@ -0,0 +1,51 @@
+﻿#ifndef TITERATOR_H
+#define TITERATOR_H
+
+#include <memory>
+#include <iostream>
+
+template <class N, class T>
+class TIterator
+{
+public:
+	using value_type = T;
+	using reference = T & ;
+	using pointer = T * ;
+	using difference_type = ptrdiff_t;
+	using iterator_category = std::forward_iterator_tag;
+
+    TIterator(std::shared_ptr<N> n) {
+        cur = n;
+    }
+
+    std::shared_ptr<N> operator* () {
+        return cur;
+    }
+
+    std::shared_ptr<T> operator-> () {
+        return cur->GetFigure();
+    }
+
+    void operator++() {
+        cur = cur->GetNext();
+    }
+
+    TIterator operator++ (int) {
+        TIterator cur(*this);
+        ++(*this);
+        return cur;
+    }
+
+    bool operator== (const TIterator &i) {
+        return (cur == i.cur);
+    }
+
+    bool operator!= (const TIterator &i) {
+        return (cur != i.cur);
+    }
+
+private:
+    std::shared_ptr<N> cur;
+};
+
+#endif
